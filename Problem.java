65dcaa9a0a6e343560cb2cd8350d8a41051dbbf1
suman-zemoso <saisuman.kopparthi@zemosolabs.com>
2017-06-08 17:32:15 +0530

    initial commit

---
+++ Problem.java
@@ -0,0 +1,11 @@
+class Dat {
+	int i;
+	char ch;
+}
+
+public class Problem {
+	public static void main(String[] args) {
+		Dat ob = new Dat();
+		System.out.println("Integer is :" +ob.i + "\nCharacter is :" + ob.ch);
+	}
+}
